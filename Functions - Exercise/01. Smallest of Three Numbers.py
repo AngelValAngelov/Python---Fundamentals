09251700fb12d9f8a0b14e907ec774d862961612
AngelValAngelov <64867029+AngelValAngelov@users.noreply.github.com>
2021-01-17 22:35:55 +0200

    Functions - Exercise

---
+++ Functions - Exercise/01. Smallest of Three Numbers.py
@@ -0,0 +1,15 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+
+
+def solve():
+    if a < b and a < c:
+        print(a)
+    elif b < a and b < c:
+        print(b)
+    elif c < a and c < b:
+        print(c)
+
+solve()
